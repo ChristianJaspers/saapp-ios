c088556873b0ac4a06da8748b96ce81e6b7d98bc
Sebastian Ewak <sebastian.ewak@gmail.com>
2014-07-17 16:25:40 +0200

    Implemented saving in-memory image to temporary file and upload

--- BetterSalesman.iOS/src/controllers/profile/ProfileViewController.cs
+++ BetterSalesman.iOS/src/controllers/profile/ProfileViewController.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using BetterSalesman.Core.ServiceAccessLayer;
 using BetterSalesman.Core.BusinessLayer;
 using BetterSalesman.Core.BusinessLayer.Managers;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace BetterSalesman.iOS
 {
@@ -33,30 +35,30 @@ namespace BetterSalesman.iOS
 				ShowImagePickerTypeSelection();
 			};
 
-			LoadUser();
+//			LoadUser();
 		}
 
 		public override void ViewDidAppear(bool animated)
 		{
 			base.ViewDidAppear(animated);
 
-			ShowIndicator();
-
-			ServiceProviderUser.Instance.Profile(async result =>
-				{
-					await UserSessionManager.Instance.FetchUser(obj =>
-					{
-						LoadUser();
-						// TODO refresh UI here?
-					});
-
-					HideIndicator();
-				},
-				errorCode =>
-				{
-					HideIndicator();  
-					ShowAlert(I18n.ErrorConnectionTimeout);
-				});
+//			ShowIndicator();
+//
+//			ServiceProviderUser.Instance.Profile(async result =>
+//				{
+//					await UserSessionManager.Instance.FetchUser(obj =>
+//					{
+//						LoadUser();
+//						// TODO refresh UI here?
+//					});
+//
+//					HideIndicator();
+//				},
+//				errorCode =>
+//				{
+//					HideIndicator();  
+//					ShowAlert(I18n.ErrorConnectionTimeout);
+//				});
 		}
 
 		private void ShowImagePickerTypeSelection()
@@ -122,17 +124,9 @@ namespace BetterSalesman.iOS
 			}
 			imagePicker.MediaTypes = UIImagePickerController.AvailableMediaTypes(pickerType);
 
-			imagePicker.FinishedPickingMedia += (object s, UIImagePickerMediaPickedEventArgs e) => 
+			imagePicker.FinishedPickingMedia += async (s, e) =>
 			{
-				Debug.WriteLine("MediaUrl: " + e.MediaUrl);
-				if (e.MediaType == ImageMediaType)
-				{
-					ProfileImageView.Image = e.OriginalImage;
-
-					// TODO - upload
-				}
-
-				imagePicker.DismissViewController(true, null);
+				await HandleFilePickedAndLoaded(s, e);
 			};
 
 			imagePicker.Canceled += (object s, EventArgs e) => 
@@ -145,6 +139,93 @@ namespace BetterSalesman.iOS
 			imagePicker.ModalTransitionStyle = UIModalTransitionStyle.CoverVertical;
 
 			PresentViewController(imagePicker, true, null);
+		}
+
+		private async Task HandleFilePickedAndLoaded(object s, UIImagePickerMediaPickedEventArgs e)
+		{
+			imagePicker.DismissViewController (true, null);
+
+			Debug.WriteLine("MediaUrl: " + e.MediaUrl);
+			if (e.MediaType == ImageMediaType)
+			{
+				await UploadImage(e.OriginalImage);
+			}
+		}
+
+		private async Task UploadImage(UIImage image)
+		{
+			var imageFilePath = await SaveImageToTemporaryFilePng(image);
+
+			var fileUploadRequest = new FileUploadRequest ();
+
+			fileUploadRequest.ProgressUpdated += (int progressPercentage) => Debug.WriteLine ("Upload progress: " + progressPercentage);
+
+			fileUploadRequest.Success += async (string remoteFileUrl) => 
+			{
+				Debug.WriteLine ("Upload complete - file URL: " + remoteFileUrl);
+				RemoveTemporaryFile(imageFilePath);
+
+				UIImage remoteImage = await Task<UIImage>.Run(() =>
+				{
+					// TODO - check if URL is valid and if connection is available
+					var data = NSData.FromUrl(new NSUrl(remoteFileUrl));
+					return UIImage.LoadFromData(data);
+				});
+
+				InvokeOnMainThread(() =>
+				{
+					ProfileImageView.Image = remoteImage;
+				});
+			};
+
+			fileUploadRequest.Failure += (int errorCode) =>
+			{
+				Debug.WriteLine ("Upload failed: " + errorCode);
+				RemoveTemporaryFile(imageFilePath);
+			};
+
+			fileUploadRequest.Perform(imageFilePath);
+		}
+
+		/// <summary>
+		/// Saves the image to temporary file in png format.
+		/// </summary>
+		/// <returns>Path to temporary file representing image provide as paramater</returns>
+		/// <param name="image">The image to be saved to a tempoarary png file</param>
+		public async Task<string> SaveImageToTemporaryFilePng(UIImage image)
+		{
+			var uniqueFileNamePortion = Guid.NewGuid().ToString();
+			var temporaryImageFileName = string.Format("{0}.png", uniqueFileNamePortion);
+
+			var documentsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var temporaryStorageFolderPath = Path.Combine(documentsFolderPath, "..", "tmp");
+
+			var temporaryImageFilePath = Path.Combine(temporaryStorageFolderPath, temporaryImageFileName);
+
+			var imageData = image.AsPNG();
+			await Task.Run(() => 
+			{
+				//File.WriteAllBytes(temporaryImageFilePath, imageData);
+				NSError error = null;
+				if (imageData.Save(temporaryImageFilePath, false, out error)) 
+				{
+					Console.WriteLine("saved file");
+				} else 
+				{
+					Console.WriteLine("NOT saved file because" + error.LocalizedDescription);
+				}
+			});
+
+			return temporaryImageFilePath;
+		}
+
+		// TODO - might require some extra checks to see if file can we opened in ReadWrite mode
+		public void RemoveTemporaryFile(string temporaryFilePath)
+		{
+			if (File.Exists(temporaryFilePath))
+			{
+				File.Delete(temporaryFilePath);
+			}
 		}
 			
 		void LoadUser()
