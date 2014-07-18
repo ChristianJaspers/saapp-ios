7c7938c967581bd2be0f63438ec1ab8b79455be3
Sebastian Ewak <sebastian.ewak@gmail.com>
2014-07-18 10:24:17 +0200

    Refactored ProfileViewController, extracted temporary file management and media picker presentation logic

---
+++ BetterSalesman.iOS/src/helpers/ImageFilesManagementHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+
+namespace BetterSalesman.iOS
+{
+	public class ImageFilesManagementHelper
+	{
+		static ImageFilesManagementHelper sharedInstance;
+		static object locker = new Object();
+
+		public static ImageFilesManagementHelper SharedInstance
+		{
+			get
+			{
+				if (sharedInstance == null)
+				{
+					lock (locker)
+					{
+						if (sharedInstance == null)
+						{
+							sharedInstance = new ImageFilesManagementHelper();
+						}
+					}
+				}
+
+				return sharedInstance;
+			}
+		}
+
+		public ImageFilesManagementHelper()
+		{
+		}
+
+		/// <summary>
+		/// Saves the image to temporary file in png format.
+		/// </summary>
+		/// <returns>Path to temporary file representing image provide as paramater</returns>
+		/// <param name="image">The image to be saved to a tempoarary png file</param>
+		public async Task<string> SaveImageToTemporaryFilePng(UIImage image)
+		{
+			var uniqueFileNamePortion = Guid.NewGuid().ToString();
+			var temporaryImageFileName = string.Format("{0}.png", uniqueFileNamePortion);
+
+			var documentsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var temporaryStorageFolderPath = Path.Combine(documentsFolderPath, "..", "tmp");
+
+			var temporaryImageFilePath = Path.Combine(temporaryStorageFolderPath, temporaryImageFileName);
+
+			var imageData = image.AsPNG();
+			await Task.Run(() => 
+			{
+				//File.WriteAllBytes(temporaryImageFilePath, imageData);
+				NSError error = null;
+				if (imageData.Save(temporaryImageFilePath, false, out error)) 
+				{
+					Console.WriteLine("saved file");
+				} else 
+				{
+					Console.WriteLine("ERROR! Did NOT SAVE file because" + error.LocalizedDescription);
+				}
+			});
+
+			return temporaryImageFilePath;
+		}
+
+		// TODO - might require some extra checks to see if file can we opened in ReadWrite mode
+		/// <summary>
+		/// Removes the temporary file at given path. 
+		/// 
+		/// NOTE! Use this method only to remove the files that were created using SaveImageToTemporaryFilePng with the path returned by that method.
+		/// </summary>
+		/// <param name="temporaryFilePath">Temporary file path.</param>
+		public void RemoveTemporaryFile(string temporaryFilePath)
+		{
+			if (File.Exists(temporaryFilePath))
+			{
+				File.Delete(temporaryFilePath);
+			}
+		}
+	}
+}
+
---
+++ BetterSalesman.iOS/src/helpers/ImagePickerPresenter.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BetterSalesman.iOS
+{
+	/// <summary>
+	/// Finished picking event handler.
+	/// </summary>
+	public delegate void FinishedPickingEventHandler(bool didPickAnImage, UIImage pickedImage);
+
+	/// <summary>
+	/// This class is responsible for configuring and displaying UIActionSheet followed by UIImagePickerController.
+	/// </summary>
+	public class ImagePickerPresenter
+	{
+		public const string ImageMediaType = "public.image";
+
+		public const UIImagePickerControllerSourceType CameraPickerType = UIImagePickerControllerSourceType.Camera;
+		public const UIImagePickerControllerSourceType PhotoLibraryPickerType = UIImagePickerControllerSourceType.PhotoLibrary;
+		public const UIImagePickerControllerSourceType SavedPhotosPickerType = UIImagePickerControllerSourceType.Camera;
+
+		public event FinishedPickingEventHandler FinishedPicking;
+
+		private UIImagePickerController imagePicker;
+		private UIActionSheet mediaSourceTypePicker;
+
+		// TODO - add to localized strings
+		private string CancelButtonLabel = "Cancel".t();
+		private string CameraButtonLabel = "Camera".t();
+		private string PhotoLibraryButtonLabel = "Photo library".t();
+		private string SavedPhotosButtonLabel = "Saved photos".t();
+		private string MediaSourceTypePickerLabel = "Choose source".t();
+
+		private Dictionary<int, UIImagePickerControllerSourceType> buttonIndexesForMediaSourceTypes;
+		private int lastAddedMediaSourceTypeButtonIndex;
+
+		public ImagePickerPresenter()
+		{
+			buttonIndexesForMediaSourceTypes = new Dictionary<int, UIImagePickerControllerSourceType>();
+		}
+
+		public void ShowImagePickerTypeSelection(UIViewController viewController)
+		{
+			buttonIndexesForMediaSourceTypes.Clear();
+			lastAddedMediaSourceTypeButtonIndex = -1;
+
+			mediaSourceTypePicker = new UIActionSheet(MediaSourceTypePickerLabel);
+
+			AddButtonToMediaSourceTypePickerIfTypeAvailable(CameraButtonLabel, CameraPickerType);
+			AddButtonToMediaSourceTypePickerIfTypeAvailable(PhotoLibraryButtonLabel, PhotoLibraryPickerType);
+			AddButtonToMediaSourceTypePickerIfTypeAvailable(SavedPhotosButtonLabel, SavedPhotosPickerType);
+			mediaSourceTypePicker.AddButton(CancelButtonLabel);
+
+			mediaSourceTypePicker.Clicked += (object sender, UIButtonEventArgs e) =>
+			{
+				if (e.ButtonIndex <= lastAddedMediaSourceTypeButtonIndex)
+				{
+					var selectedMediaSourceType = buttonIndexesForMediaSourceTypes[e.ButtonIndex];
+					ShowImagePicker(viewController, selectedMediaSourceType);
+				}
+			};
+
+			mediaSourceTypePicker.ShowInView(viewController.View);
+		}
+
+		private void ShowImagePicker(UIViewController viewController, UIImagePickerControllerSourceType pickerType)
+		{
+			imagePicker = new UIImagePickerController();
+
+			imagePicker.SourceType = pickerType;
+			if (pickerType == UIImagePickerControllerSourceType.Camera)
+			{
+				imagePicker.ShowsCameraControls = true;
+			}
+			imagePicker.MediaTypes = UIImagePickerController.AvailableMediaTypes(pickerType);
+
+			imagePicker.FinishedPickingMedia += (s, e) =>
+			{
+				Debug.WriteLine("Finished picking");
+				viewController.InvokeOnMainThread(() =>
+				{
+					bool didPickAnImage = e.MediaType == ImageMediaType && e.OriginalImage != null;
+					OnFinishedPicking(didPickAnImage, e.OriginalImage);
+				});
+			};
+
+			imagePicker.Canceled += (object s, EventArgs e) => 
+			{
+				OnFinishedPicking(false, null);
+			};
+
+			imagePicker.ModalInPopover = true;
+			imagePicker.ModalPresentationStyle = UIModalPresentationStyle.FullScreen;
+			imagePicker.ModalTransitionStyle = UIModalTransitionStyle.CoverVertical;
+
+			viewController.PresentViewController(imagePicker, true, null);
+		}
+
+		private void OnFinishedPicking(bool didPickAnImage, UIImage pickedImage)
+		{
+			Debug.WriteLine("Finished picking: " + pickedImage);
+			imagePicker.DismissViewController(true, null);
+			if (FinishedPicking != null)
+			{
+				FinishedPicking(didPickAnImage, pickedImage);
+			}
+		}
+
+		private bool IsPickerTypeAvailable(UIImagePickerControllerSourceType type)
+		{
+			return UIImagePickerController.IsSourceTypeAvailable(type);
+		}
+
+		private void AddButtonToMediaSourceTypePickerIfTypeAvailable(string buttonLabel, UIImagePickerControllerSourceType mediaSourceType)
+		{
+			if (IsPickerTypeAvailable(mediaSourceType))
+			{
+				mediaSourceTypePicker.AddButton(buttonLabel);
+				lastAddedMediaSourceTypeButtonIndex += 1;
+				buttonIndexesForMediaSourceTypes.Add(lastAddedMediaSourceTypeButtonIndex, mediaSourceType);
+			}
+		}
+	}
+}
+
