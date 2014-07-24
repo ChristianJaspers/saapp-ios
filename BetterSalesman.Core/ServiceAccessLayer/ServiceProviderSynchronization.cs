25ddfee17d92eebc6986c7e1c86198fe5416581e
Dawid Pośliński <dposlinski@gmail.com>
2014-07-24 09:12:20 +0200

    Sync service provider

---
+++ BetterSalesman.Core/ServiceAccessLayer/ServiceProviderSynchronization.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BetterSalesman.Core.ServiceAccessLayer
+{
+    public class ServiceProviderSynchronization : BaseServiceProvider
+    {
+        private static ServiceProviderSynchronization instance;
+        private static object locker = new Object();
+        
+        // Paths
+        const string pathSynchronization = "api/json/get/bOYtHYkYAy?indent=2";
+        
+        public static ServiceProviderSynchronization Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (locker)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new ServiceProviderSynchronization();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+        
+        #region Requests
+        
+        public async void Synchronize(
+            HttpRequestSuccessEventHandler success = null, 
+            HttpRequestFailureEventHandler failure = null
+        )
+        {
+            var request = new HttpRequest {
+                Method = HTTPMethod.GET,
+                Path = pathSynchronization,
+                Parameters = ParametersWithDeviceInfo(new Dictionary<string, object>())
+            };
+            
+            request.Success += result => {
+                
+                // TODO parse synchronization request
+
+                if ( success != null )
+                {
+                    success(result);
+                }
+            };
+
+            request.Failure += failure;
+            
+            await request.Perform();
+        }
+        
+        #endregion
+
+        #region Json definitions
+
+        class ResponseJsonSynchronization
+        {
+            
+        }
+        
+        #endregion
+    }
+}
+
