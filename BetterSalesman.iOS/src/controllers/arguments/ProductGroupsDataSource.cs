785cf0ec1bc98c01f3d222a911d1c0110fe526fe
Sebastian Ewak <sebastian.ewak@gmail.com>
2014-07-31 15:34:39 +0200

    Added TableViewDataSource implementation to ProductGroupsDataSource

--- BetterSalesman.iOS/src/controllers/arguments/ProductGroupsDataSource.cs
+++ BetterSalesman.iOS/src/controllers/arguments/ProductGroupsDataSource.cs
@@ -5,13 +5,17 @@ using System.Threading.Tasks;
 using BetterSalesman.Core.BusinessLayer.Managers;
 using System.Linq;
 using System.Diagnostics;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
 
 namespace BetterSalesman.iOS
 {
 	public delegate void SelectedProductGroupChanged(ProductGroup newSelectedProductGroup);
 
-	public class ProductGroupsDataSource
+	public class ProductGroupsDataSource : UITableViewSource
 	{
+		private const string cellIdentifierItem = "cellProductGroup";
+
 		public event SelectedProductGroupChanged SelectedProductGroupChanged;
 
 		private ProductGroup selectedProductGroup;
@@ -44,28 +48,6 @@ namespace BetterSalesman.iOS
 		{
 			ProductGroups = ProductGroupManager.GetProductGroups();
 
-			#if DEBUG
-			Debug.WriteLine("DEBUG MODE...");
-			if (ProductGroups == null || ProductGroups.Count <= 0)
-			{
-				ProductGroups = new List<ProductGroup>
-				{
-					new ProductGroup
-					{
-						Name = "Test group #1"
-					},
-					new ProductGroup
-					{
-						Name = "Test group #2"
-					},
-					new ProductGroup
-					{
-						Name = "Test group #3"
-					}
-				};
-			}
-			#endif
-
 			SelectedProductGroup = ProductGroups.FirstOrDefault();
 		}
 
@@ -75,6 +57,26 @@ namespace BetterSalesman.iOS
 			{
 				SelectedProductGroupChanged(newSelectedProductGroup);
 			}
+		}
+
+		public override int RowsInSection(UITableView tableview, int section)
+		{
+			return ProductGroups.Count;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			var cell = tableView.DequeueReusableCell(cellIdentifierItem) ?? new UITableViewCell();
+
+			var featureTxt = (UILabel)cell.ViewWithTag(1);
+			var benefitTxt = (UILabel)cell.ViewWithTag(2);
+
+			var relevanceTxt = (UILabel)cell.ViewWithTag(5);
+			var verticalLine = (UIView)cell.ViewWithTag(6);
+
+			var productGroup = ProductGroups[indexPath.Row];
+
+			return cell;
 		}
 	}
 }
